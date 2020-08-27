bbe6b3635ec4ad23ed43f213c46792ec77e56b68
SaranshDawra <saransh2000dawra@gmail.com>
2020-08-27 22:39:35 +0530

    index.js added

---
+++ index.js
@@ -0,0 +1,28 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const express = require("express");
+const mongoose = require('mongoose');
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
+const app = express();
+
+mongoose
+    .connect("mongodb://localhost/vidly", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
