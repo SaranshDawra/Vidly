44049cf390a78bcf2e4ea2fb4a3849dba31db9bb
SaranshDawra <saransh2000dawra@gmail.com>
2020-09-01 23:38:21 +0530

    async middleware added

---
+++ middleware/async.js
@@ -0,0 +1,11 @@
+function asyncMiddleware(handler) {
+    return async (req, res, next) => {
+        try {
+            await handler(req, res);
+        } catch (err) {
+            next(err);
+        }
+    };
+}
+
+module.exports = asyncMiddleware;
